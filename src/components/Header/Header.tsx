6419cdfe8e4095a825b75bebabfa9b7df2a8c8e7
Jeet Panchal <jeetbpanchal@gmail.com>
2026-08-27 09:40:44 -0400

    refactor: separate header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,51 +1,11 @@
 "use client";
 
-import styled from "styled-components";
-import { colors, spacing } from "@/styles/tokens";
-
-const HeaderWrapper = styled.header`
-  width: 100%;
-  background: ${colors.snow};
-  color: ${colors.carbon};
-`;
-
-const HeaderInner = styled.div`
-  width: min(100%, 1312px);
-  margin: 0 auto;
-  min-height: 80px;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  padding-inline: ${spacing.sm};
-`;
-
-const LogoMark = styled.div`
-  width: 40px;
-  height: 40px;
-  border: 6px solid ${colors.carbon};
-  border-radius: 50%;
-`;
-
-const ThemeButton = styled.button`
-  width: 40px;
-  height: 40px;
-
-  display: grid;
-  place-items: center;
-
-  border: 0;
-  background: transparent;
-  color: inherit;
-
-  border-radius: ${spacing.xs};
-
-  &:focus-visible {
-    outline: 2px solid ${colors.carbon};
-    outline-offset: 2px;
-  }
-`;
+import {
+  HeaderInner,
+  HeaderWrapper,
+  LogoMark,
+  ThemeButton,
+} from "./header.styles";
 
 export function Header() {
   return (
---
+++ src/components/Header/header.styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+import { colors, spacing } from "@/styles/tokens";
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+
+  background: ${colors.snow};
+  color: ${colors.carbon};
+`;
+
+export const HeaderInner = styled.div`
+  width: min(100%, 1312px);
+  min-height: 80px;
+
+  margin: 0 auto;
+  padding-inline: ${spacing.sm};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LogoMark = styled.div`
+  width: 40px;
+  height: 40px;
+
+  border: 6px solid ${colors.carbon};
+  border-radius: 50%;
+`;
+
+export const ThemeButton = styled.button`
+  width: 40px;
+  height: 40px;
+
+  display: grid;
+  place-items: center;
+
+  border: 0;
+  border-radius: ${spacing.xs};
+
+  background: transparent;
+  color: inherit;
+
+  &:focus-visible {
+    outline: 2px solid ${colors.carbon};
+    outline-offset: 2px;
+  }
+`;
